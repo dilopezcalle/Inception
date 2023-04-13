accedc95afe8a8115aac5dc7fd59f4fe007b340e
dilopezcalle <dilopez-@42urduliz.com>
2023-04-13 10:55:01 +0200

    MariaDB conf file done

--- srcs/requirements/mariadb/conf/mariadb_script.sh
+++ srcs/requirements/mariadb/conf/mariadb_script.sh
@@ -4,32 +4,9 @@
 # creación y asignación de permisos a un usuario,
 # y cambia la contraseña del usuario root
 
-mysql_install_db
-
 # Iniciar el servicio
-/etc/init.d/mysql start
-
-mysql_secure_installation <<_EOF_
-
-Y
-$MYSQL_PASSWORD_ROOT
-$MYSQL_PASSWORD_ROOT
-Y
-n
-Y
-Y
-_EOF_
-
-# Crear un .sql con las instrucciones a ejecutar
-# echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;" > db.sql
-# echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> db.sql
-# echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ;" >> db.sql
-# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD_ROOT' ;" >> db.sql
-# echo "FLUSH PRIVILEGES;" >> db.sql
-
-# Modificar la contraseña del usuario administrador
-echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_PASSWORD_ROOT';" | mysql -uroot
-echo "FLUSH PRIVILEGES;" | mysql -uroot
+mysql_install_db
+service mysql start
 
 # Crear una nueva base de datos si no existe
 echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" | mysql -uroot
@@ -37,12 +14,13 @@ echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" | mysql -uroot
 echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" | mysql -uroot
 # Dar todos los privilegos de la base de datos creada al usuario creado
 echo "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -uroot
+# Modificar la contraseña del usuario administrador (no en localhost)
+echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_PASSWORD_ROOT' ;" | mysql -uroot
 # Actualizar los permisos
 echo "FLUSH PRIVILEGES;" | mysql -uroot
 
-# Detener el servicio
+# Detener el servicio para que aplique los cambios de contraseñas
 /etc/init.d/mysql stop
-# kill $(cat /var/run/mysqld/mysqld.pid)
 
 # Permitir que se ejecuten comandos al finalizar
 exec "$@"
